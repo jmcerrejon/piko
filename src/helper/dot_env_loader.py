ec808cad9927d7bbbd393863975583d26d82726c
Jose Cerrejon <ulysess@gmail.com>
2024-11-08 12:05:33 +0100

    feat: Add helper modules

---
+++ src/helper/dot_env_loader.py
@@ -0,0 +1,24 @@
+import os
+
+
+class DotenvLoader:
+    """
+    A class to load environment variables from a .env file.
+
+    Methods:
+        load(file_path):
+            Loads the environment variables from the .env file into the os.environ dictionary.
+
+    Usage:
+        # Accessing an environment variable
+        DotenvLoader.load(".env")
+        value = os.environ.get('VARIABLE_NAME')
+    """
+
+    @staticmethod
+    def load(file_path):
+        with open(file_path) as f:
+            for line in f:
+                if line.strip() and not line.startswith("#"):
+                    key, value = line.strip().split("=", 1)
+                    os.environ[key] = value
