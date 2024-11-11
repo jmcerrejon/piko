fde3694fe52e9685a8cf0ed55cf3f3c0678ce729
Jose Cerrejon <ulysess@gmail.com>
2024-11-11 10:41:23 +0100

    feat: Update application version and refactor header message generation

--- main.py
+++ main.py
@@ -10,30 +10,16 @@ import typing
 
 from src.ai import AI
 from src.helpers.dot_env_loader import DotenvLoader
+from src.helpers.utils import Utils
 
 DotenvLoader.load(".env")
-APP_VERSION = "1.0.ABC"
+APP_VERSION = "1.0.CDE"
 _BYE_MESSAGE = "\nBye! I hope I was helpful! Bzzz 🪰"
 DEFAULT_LIBRARY = "openai"
 
 
 def print_header() -> None:
-    print(f"""
-    d8888b. d888888b db   dD  .d88b.  
-    88  `8D   `88'   88 ,8P' .8P  Y8. 
-    88oodD'    88    88,8P   88    88 
-    88~~~      88    88`8b   88    88 
-    88        .88.   88 `88. `8b  d8' 
-    88      Y888888P YP   YD  `Y88P'  
-
-    Version: {APP_VERSION}
-
-Hi! I'm PIKO!. 🪰
-
-The new AI created from the collective mind of trillons of flies.
-
-Type [exit] to exit the program or [imagine] to get a picture about something you want.
-""")
+    print(Utils.get_header_message(APP_VERSION))
 
 
 def signal_handler(sig: int, frame: typing.Any) -> None:
@@ -54,5 +40,5 @@ if __name__ == "__main__":
             print(_BYE_MESSAGE)
             break
 
-        response = ai.answer(prompt) if prompt != "imagine" else ai.draw()
+        response = ai.answer(prompt) if prompt != "imagine" else ai.draw()  # type: ignore
         print(response)
--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -2,12 +2,17 @@ import os
 import subprocess
 import sys
 
+additional_header_message = ""
+
 
 class Utils:
     """
-    A utility class that provides various helper methods.
+    A utility class providing various helper methods.
     Methods:
         open_default_browser(url: str) -> None:
+        create_virtualenv() -> None:
+        get_header_message(app_version: str) -> str:
+            Generate a header message with the given application version.
     """
 
     @staticmethod
@@ -32,15 +37,38 @@ class Utils:
         This function checks if a virtual environment is currently active by
         inspecting the 'VIRTUAL_ENV' environment variable. If no virtual
         environment is detected, it creates a new one using the '.venv' module.
-        
+
         Returns:
             None
         """
         if os.getenv("VIRTUAL_ENV") is None:
-            activate_script = ".venv/bin/activate" if os.name != "nt" else ".venv\\Scripts\\activate"
+            activate_script = (
+                ".venv/bin/activate" if os.name != "nt" else ".venv\\Scripts\\activate"
+            )
 
             print("Virtual environment not detected.\nCreating virtual environment...")
             subprocess.check_call([sys.executable, "-m", "venv", ".venv"])
-            
-            print(f"Now activate the environment with the command: source {activate_script} and run It again.")
+
+            print(
+                f"Now activate the environment with the command: source {activate_script} and run It again."
+            )
             exit(0)
+
+    @staticmethod
+    def get_header_message(app_version: str) -> str:
+        return f"""
+    d8888b. d888888b db   dD  .d88b.  
+    88  `8D   `88'   88 ,8P' .8P  Y8. 
+    88oodD'    88    88,8P   88    88 
+    88~~~      88    88`8b   88    88 
+    88        .88.   88 `88. `8b  d8' 
+    88      Y888888P YP   YD  `Y88P'  
+
+    Version: {app_version}
+
+    Hi! I'm PIKO!. 🪰
+
+    The new AI created from the collective mind of trillons of flies.
+
+    Type [exit] to exit the program. {additional_header_message}
+    """
